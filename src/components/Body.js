ae28e626015c0df4527c22799d3d0d81e359383d
Harsh sahu <sahuharsh164@gmail.com>
2023-10-18 14:54:03 +0530

    signing form and header

---
+++ src/components/Body.js
@@ -0,0 +1,26 @@
+import Login from "./Login";
+import Browse from "./Browse";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+const Body=()=>{
+    
+    const appRouter=createBrowserRouter([
+       
+        {
+            path:'/browse',
+            element: <Browse/>
+        },
+        {
+            path:'/',
+            element: <Login/>
+        },
+    ])
+
+    return (
+        <div>
+ 
+           <RouterProvider router={appRouter}/>
+        </div>
+    )
+}
+export default Body;
